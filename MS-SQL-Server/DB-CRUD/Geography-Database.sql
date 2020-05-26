34647751cc3f9c5135fb028cad5ab505b1f7686d
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2020-05-26 21:52:02 +0300

    Adding solutions - DB-CRUD

---
+++ MS-SQL-Server/DB-CRUD/Geography-Database.sql
@@ -0,0 +1,20 @@
+SELECT [PeakName]
+FROM Peaks
+ORDER BY [PeakName] ASC
+
+SELECT TOP (30) CountryName, Population
+FROM Countries
+WHERE ContinentCode = 'EU'
+ORDER BY [Population] DESC
+
+SELECT * 
+FROM Countries
+
+SELECT CountryName, CountryCode,
+	CASE
+		WHEN CurrencyCode = 'EUR' THEN 'Euro'
+		ELSE 'Not Euro'
+	END AS [Currency]
+FROM Countries
+ORDER BY CountryName ASC
+
---
+++ MS-SQL-Server/DB-CRUD/SoftUni-DataBase.sql
@@ -0,0 +1,74 @@
+SELECT * FROM Departments
+
+SELECT [Name] FROM Departments
+
+SELECT FirstName, LastName, Salary FROM Employees
+
+SELECT FirstName, MiddleName, LastName FROM Employees
+
+SELECT FirstName + '.' + LastName + '@' + 'softuni.bg' AS [Full Name]      
+  	FROM Employees 
+
+SELECT DISTINCT Salary FROM Employees
+
+SELECT *
+	FROM Employees
+	WHERE JobTitle = 'Sales Representative'
+
+SELECT FirstName, LastName, JobTitle 
+FROM Employees
+WHERE Salary >= 20000 AND Salary <= 30000
+
+SELECT FirstName + ' ' + MiddleName + ' ' + LastName AS [Full Name]      
+  	FROM Employees
+	WHERE Salary IN (25000, 14000, 12500, 23600)
+
+SELECT FirstName, LastName
+FROM Employees
+WHERE ManagerID IS NULL
+
+SELECT FirstName, LastName, Salary 
+FROM Employees
+WHERE Salary > 50000 
+ORDER BY Salary DESC
+
+SELECT TOP (5) FirstName, LastName
+FROM Employees
+ORDER BY Salary DESC
+
+SELECT FirstName, LastName
+FROM Employees
+WHERE DepartmentID != 4
+
+SELECT *
+FROM Employees
+ORDER BY Salary DESC, FirstName ASC, LastName DESC, MiddleName ASC
+GO
+
+CREATE VIEW V_EmployeesSalaries AS
+SELECT FirstName, LastName, Salary
+FROM Employees
+GO
+
+CREATE VIEW V_EmployeeNameJobTitle AS
+SELECT FirstName + ' ' + ISNULL(MiddleName, '') + ' ' + LastName AS [Full Name], JobTitle
+FROM Employees
+GO
+
+SELECT DISTINCT JobTitle
+FROM Employees
+
+SELECT * FROM Projects
+
+SELECT TOP(10) *
+FROM Projects
+ORDER BY StartDate, [Name]
+
+SELECT TOP(7) FirstName, LastName, HireDate
+FROM Employees
+ORDER BY HireDate DESC
+
+UPDATE Employees
+SET Salary = Salary * 1.12       
+WHERE DepartmentID IN (1, 2, 4, 11)	
+SELECT Salary FROM Employees
